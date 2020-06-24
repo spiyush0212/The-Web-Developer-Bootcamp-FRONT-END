0cf1060d6ef8402db3634c35e4c2fda96d2c4291
Piyush Sharma <54810697+spiyush0212@users.noreply.github.com>
2020-06-24 08:21:59 +0530

    Add files via upload

---
+++ JavaScript-Intro/1-JS-Demo.js
@@ -0,0 +1,3 @@
+var name = prompt("Hello! What is your name?");
+alert("Have a great day, " + name);
+console.log("You are here too Mr. " + name);
---
+++ JavaScript-Intro/a12-js-stalker.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+alert("Your name is " + firstName + " " + lastName);
+alert("Your age is " + age);
+
+console.log("Your name is " + firstName + " " + lastName);
+console.log("Your age is " + age);
